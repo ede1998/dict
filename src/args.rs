a3f1425c25f4b43b8199df07bc5545e43b4a8c96
ede1998 <online@erik-hennig.me>
2019-08-19 20:57:43 +0200

    default language value from single constant, also when displaying

--- src/args.rs
+++ src/args.rs
@@ -2,24 +2,33 @@ use clap::{Arg, ArgMatches};
 
 use crate::translator;
 use translator::Language;
+use translator::DEFAULT_LANGUAGES;
 
 pub const LANGUAGE_PAIR: &str = "language pairs";
 pub const QUERY: &str = "query word";
 
 pub fn parse() -> ArgMatches<'static> {
     let matches = app_from_crate!()
-        .arg(Arg::with_name(LANGUAGE_PAIR)
+        .arg(
+            Arg::with_name(LANGUAGE_PAIR)
                 .short("l")
                 .long("languages")
                 .number_of_values(2)
-                .help("Languages to translate between (default: deen)")
+                .help(&format!(
+                    "Languages to translate between (default: {}-{})",
+                    DEFAULT_LANGUAGES.0.get_abbreviation(),
+                    DEFAULT_LANGUAGES.1.get_abbreviation()
+                ))
                 .takes_value(true)
                 .case_insensitive(true)
-                .possible_values(&Language::get_all_language_abbreviations()[..]))
-        .arg(Arg::with_name(QUERY)
+                .possible_values(&Language::get_all_language_abbreviations()[..]),
+        )
+        .arg(
+            Arg::with_name(QUERY)
                 .help("The word that should be translated")
                 .required(true)
-                .index(1))
+                .index(1),
+        )
         .get_matches();
 
     matches
